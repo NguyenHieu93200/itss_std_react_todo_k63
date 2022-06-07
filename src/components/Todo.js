66eb72e56bbc5117d942e2e0a5a058dfcffb59cf
NguyenHieu93200 <hieu.nm932000@gmail.com>
2022-06-07 14:41:00 +0000

    Add Tabs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -26,6 +26,14 @@ function Todo() {
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
+  const [filter, setFilter] = React.useState(['ALL']);
+
+  const displayItems = items.filter(item => {
+    if(filter === 'ALL') return true
+    if(filter === 'TODO') return !item.done
+    if(filter === 'DONE') return item.done
+  });
+  
   const handleCheck = checked => {
     const newItems = items.map(item => {
       if (item.key === checked.key) {
@@ -35,9 +43,12 @@ function Todo() {
     });
     putItems(newItems);
   };
+  
   const handleAdd = text => {
     putItems([...items, { key: getKey(), text, done: false }]);
   };
+  
+  const handleFilterChange = value => setFilter(value) 
 
   return (
     <div className="panel">
@@ -45,7 +56,11 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       <Input onAdd={handleAdd}/>
-      {items.map(item => (
+      <Filter 
+        onChange = {handleFilterChange}  
+        value={filter}
+      />
+      {displayItems.map(item => (
         <TodoItem
           onCheck = {handleCheck}
           key={item.key}
@@ -53,7 +68,7 @@ function Todo() {
         />  
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length === 0 ? items.length : displayItems.length} items
       </div>
     </div>
   );
